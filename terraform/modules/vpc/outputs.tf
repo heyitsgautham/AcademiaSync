406979acc60ecc22580572844f472d73c8885ea5
Gautham Krishna <heyitsgautham@gmail.com>
2025-10-13 22:22:00 +0530

    implemented terraform infrastructure for aws deployment with default vpc

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,21 +1,21 @@
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = aws_vpc.main.id
+  value       = data.aws_vpc.main.id
 }
 
 output "vpc_cidr" {
   description = "CIDR block of the VPC"
-  value       = aws_vpc.main.cidr_block
+  value       = data.aws_vpc.main.cidr_block
 }
 
 output "public_subnet_ids" {
   description = "IDs of public subnets"
-  value       = aws_subnet.public[*].id
+  value       = data.aws_subnets.public.ids
 }
 
 output "internet_gateway_id" {
   description = "ID of Internet Gateway"
-  value       = aws_internet_gateway.main.id
+  value       = data.aws_internet_gateway.main.id
 }
 
 output "ecs_security_group_id" {
